dd16d480d03356da674f81c0deda3330ff8e1a08
konyan <devkonyan@gmail.com>
2020-05-29 01:37:33 +0630

    delete post

--- src/app.js
+++ src/app.js
@@ -32,7 +32,27 @@ function submitPost() {
     .catch((error) => console.log(error));
 }
 
+function deletePost(e) {
+  e.preventDefault();
+  console.log("EVENT", e);
+  if (e.target.parentElement.classList.contains("delete")) {
+    const id = e.target.parentElement.dataset.id;
+    console.log(id);
+    if (confirm("ARE YOU SURE?")) {
+      http
+        .delete(URL + "/" + id)
+        .then((data) => {
+          ui.showAlert("Post removed", "alert alert-danger");
+          getPosts();
+        })
+        .catch((error) => console.log(error));
+    }
+  }
+}
+
 //GET POSTS on DOM LOAD
 document.addEventListener("DOMContentLoaded", getPosts);
 //LISTEN BUTTON CLICK
 document.querySelector(".post-submit").addEventListener("click", submitPost);
+//DELETE LIST CLICK
+document.querySelector("#posts").addEventListener("click", deletePost);
